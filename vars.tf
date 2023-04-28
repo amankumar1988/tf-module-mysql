c1cdf3ae15aac89bf54462eb610f14a848a4aeb6
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-28 23:22:01 +0530

    Auto Push

--- vars.tf
+++ vars.tf
@@ -1,6 +1,6 @@
 variable "ENV" {}
 variable "MYSQL_RDS_PORT" {}
-varible "MYSQL_RDS_STORAGE" {}
+variable "MYSQL_RDS_STORAGE" {}
 variable "MYSQL_RDS_VERSION" {}
 variable "MYSQL_RDS_INSTANCE_TYPE" {}
 
